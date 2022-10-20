988806682b4b7d27e70a8d8592bcb69db23d045f
Sumit8420 <sumit.kumar@bridgentech.com>
2022-10-20 18:55:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import AddCard from './components/AddCard';
+import Modals from './components/Modal';
+
+function App() {
+  // <AddCard/>
+  return (
+    <div className="App">
+      {/* <Modals/> */}
+      <AddCard/>
+    </div>
+  );
+}
+
+export default App;
